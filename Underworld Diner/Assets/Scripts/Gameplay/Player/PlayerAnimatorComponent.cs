a5a5d1a51cb32b389e29e8b4a4ed3929dcde4906
trush <trushchakolga@gmail.com>
2025-04-24 22:54:52 +0300

    Add direct movement idle and walking animations

---
+++ Underworld Diner/Assets/Scripts/Gameplay/Player/PlayerAnimatorComponent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+namespace Gameplay.Player
+{
+    public class PlayerAnimatorComponent : IInitializable
+    {
+        [Inject] private Animator _animator;
+        [Inject] private PlayerInput _playerInput;
+
+        private InputAction _moveAction;
+        private Vector2 _moveInput;
+
+        public void Initialize()
+        {
+            _moveAction = _playerInput.actions.FindAction("Move");
+            _moveAction.started += StartMove;
+            _moveAction.performed += Move;
+            _moveAction.canceled += StopMove;
+        }
+        public void Move(InputAction.CallbackContext context)
+        {
+            _moveInput = context.ReadValue<Vector2>();
+            Debug.Log(_moveInput);
+            _animator.SetFloat("InputX", _moveInput.x);
+            _animator.SetFloat("InputY", _moveInput.y);
+        }
+
+        public void StopMove(InputAction.CallbackContext context)
+        {
+            _animator.SetBool("isMoving", false);
+        }
+        
+        private void StartMove(InputAction.CallbackContext obj)
+        {
+            _animator.SetBool("isMoving", true);
+        }
+
+
+    }
+}
--- Underworld Diner/Assets/Scripts/Gameplay/Player/PlayerMonoInstaller.cs
+++ Underworld Diner/Assets/Scripts/Gameplay/Player/PlayerMonoInstaller.cs
@@ -7,10 +7,13 @@ public class PlayerMonoInstaller : MonoInstaller
 {
     [SerializeField] private PlayerInput _playerInput;
     [SerializeField] private Rigidbody2D _playerRigidbody;
+    [SerializeField] private Animator _animator;
     public override void InstallBindings()
     {
         Container.BindInstance(_playerInput).AsSingle();
         Container.BindInstance(_playerRigidbody).AsSingle();
+        Container.BindInstance(_animator).AsSingle();
         Container.BindInterfacesAndSelfTo<PlayerMovementComponent>().AsSingle();
+        Container.BindInterfacesAndSelfTo<PlayerAnimatorComponent>().AsSingle();
     }
 }
